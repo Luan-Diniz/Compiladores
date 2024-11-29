acdac01e7762989cc5acf7a38125a0486306cc48
Tiago-O-Luz <tiagoluz64@gmail.com>
2024-11-29 19:36:02 -0300

    analisador lexico

---
+++ compiler/symbol_table.py
@@ -0,0 +1,20 @@
+
+
+class SymbolTable():
+    def __init__(self) -> None:
+        self.__table: dict[str, dict[str,]] = {}
+
+    def add(self, symbol: str, line: int, column: int):
+        entry = self.__table.get(symbol, None)
+        if entry:
+            self.__table[symbol]["occurrences"].append((line,column))
+        else:
+            self.__table[symbol] = {
+                "occurrences": [(line, column)]
+            }
+            
+    def get(self, symbol: str):
+        return self.__table.get(symbol, None)
+
+    def get_all_ocurrences(self):
+        return self.__table
