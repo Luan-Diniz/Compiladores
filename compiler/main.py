a1007d95ad1ea68a84cd7e361df747d33bf4a158
Luan-Diniz <luan.diniz.moraes@hotmail.com>
2024-12-05 20:05:46 -0300

    Começo analisador sintatico

--- compiler/main.py
+++ compiler/main.py
@@ -1,11 +1,15 @@
 from lexical_analyzer.lexical_analyzer import LexicalAnalyzer
+from syntactic_analyzer.syntactic_analyzer import SyntaticAnalyzer
 from symbol_table import SymbolTable
 
 la = LexicalAnalyzer()
+sa = SyntaticAnalyzer()
 
-for output in la.analyze():
-    print(output)
+for lexem, token in la.analyze():
     
+    result = sa.analyze(lexem, token)
+    
+    print(result)
     # ToDo: Integrar com o analisador sintatico
 
 #print(SymbolTable().get_all_occurrences()) # Teste Singleton
--- compiler/syntactic_analyzer/syntactic_analyzer.py
+++ compiler/syntactic_analyzer/syntactic_analyzer.py
@@ -1,70 +1,42 @@
-from  constants import *
-
-'''
-
-# ToDo
-
-import os
-
-def get_userdef_terminals() -> list:
-    try:
-        # Coleta os caracteres da tabela de símbolos até encontrar um ":"
-        with open(SYMBOL_TABLE_FILE, 'r') as symbol_table:
-            terminals = [line.split(':')[0].strip() for line in symbol_table]
-            return terminals
-    except:
-        print("Error opening files!")
-        return []
+from constants import *
+from .ll1_table import LL1_PARSING_TABLE
 
 
-class SyntacticAnalyzer:
-    def __init__(self):
-        pass
+class SyntaticAnalyzer():
+    #self._symbol_table = S
 
-    def analyze(self) -> bool:
-        # Abre os arquivos de entrada e saída
-        if not os.path.isfile(INPUT_FILE):
-            print("Input file does not exist!")
-            return False
-        try:
-            inputFile = open(INPUT_FILE, 'r')
-            outputFile = open(OUTPUT_FILE, 'w')
-        except IOError:
-            print("Error opening files!")
-            return False
+    def analyze(self, lexem, token):
 
-        # w = input_file
-        w = w.replace("0M", "a").replace("1M", "b").replace("0m", "c").replace("1m", "d")
-        w += "$"
-        
-        print(w)
+        if token == IDENTIFIER_TOKEN and lexem not in RESERVED_WORDS:
+            w = "ident" + "$"
+        else:
+            w = lexem + "$"
+
+        print(w, end='   Result:')
 
         i = 0
         pilha = ["S", "$"]
 
         X = pilha[0]
         while X != "$":
+            try:
+                if X == w[i]:
+                    pilha.pop(0)
+                    i += 1
+                elif X in TERMINALS or LL1_PARSING_TABLE.get((X, w[i])) == None:
+                    return False
+                    
+                else:
+                    #print(f"({X}, {w[i]}) -> {LL1_PARSING_TABLE[(X, w[i])][1]}")
+                    pilha.pop(0)
 
-            if X == w[i]:
-                pilha.pop(0)
-                i += 1
-            elif X in TERMINALS or LL1_PARSING_TABLE[(X, w[i])] == '':
-                return False
-                
-            else:
-                print(f"({X}, {w[i]}) -> {LL1_PARSING_TABLE[(X, w[i])][1]}"
-                    .replace("a", "0M").replace("b", "1M").replace("c", "0m")
-                    .replace("d", "1m").replace("C", "B'"))
-                pilha.pop(0)
-
-                for caracter in reversed(list(LL1_PARSING_TABLE[(X, w[i])][1])):
+                    for caracter in reversed(list(LL1_PARSING_TABLE[(X, w[i])][1])):     # LOGICA ERRADA --> Usar split(' ')??
+                    if caracter == 'epslon' or '': 
+                        continue
                     pilha.insert(0, caracter)
+            except KeyError:
+                return False
             
             X = pilha[0]
 
-        # Fecha arquivos e escreve a tabela de símbolos
-        inputFile.close()
-        outputFile.close()
-
         return True
-'''
