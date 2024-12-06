a3830b5e259dbf6c15784ddb972659f51de71a53
Luan-Diniz <luan.diniz.moraes@hotmail.com>
2024-12-06 10:56:50 -0300

    Mais implementação. Ainda não funciona.

--- compiler/main.py
+++ compiler/main.py
@@ -1,15 +1,33 @@
 from lexical_analyzer.lexical_analyzer import LexicalAnalyzer
 from syntactic_analyzer.syntactic_analyzer import SyntaticAnalyzer
 from symbol_table import SymbolTable
+from syntactic_analyzer.ll1_table import LL1_PARSING_TABLE # for debug
+from constants import *
 
 la = LexicalAnalyzer()
 sa = SyntaticAnalyzer()
 
-for lexem, token in la.analyze():
-    
-    result = sa.analyze(lexem, token)
-    
+lexical_iterator = la.analyze()
+lexem, token = next(lexical_iterator)
+syntactic_iterator = sa.analyze(lexem, token)
+
+if next(syntactic_iterator) == SyntacticProcessing.FAILED:
+        print('Erro sintático ocorreu')
+        exit()
+
+for lexem, token in lexical_iterator:
+    #print(f"Lexema: {lexem}, Token: {token}")
+
+    result = syntactic_iterator.send((lexem, token)) 
+
+
     print(result)
+    continue  # should be deleted
+    if result == SyntacticProcessing.FAILED:
+        print('Erro sintático ocorreu')
+        exit()
+
+
     # ToDo: Integrar com o analisador sintatico
 
 #print(SymbolTable().get_all_occurrences()) # Teste Singleton
--- compiler/syntactic_analyzer/syntactic_analyzer.py
+++ compiler/syntactic_analyzer/syntactic_analyzer.py
@@ -7,36 +7,42 @@ class SyntaticAnalyzer():
 
     def analyze(self, lexem, token):
 
-        if token == IDENTIFIER_TOKEN and lexem not in RESERVED_WORDS:
-            w = "ident" + "$"
-        else:
-            w = lexem + "$"
-
-        print(w, end='   Result:')
-
-        i = 0
         pilha = ["S", "$"]
-
+        w = ''
         X = pilha[0]
         while X != "$":
+            if  w != "$":
+                if (token == IDENTIFIER_TOKEN) and (lexem not in RESERVED_WORDS):  
+                    w = "ident" 
+                else:
+                    w = lexem 
+            print(f'word: {w}', end = '  ')
+            #print(f"Word: {w}", end= '  \n') # deletme
+
+
             try:
-                if X == w[i]:
+                if X == w:
                     pilha.pop(0)
-                    i += 1
-                elif X in TERMINALS or LL1_PARSING_TABLE.get((X, w[i])) == None:
-                    return False
+                elif X in TERMINALS or LL1_PARSING_TABLE.get((X, w)) == None:
+                    yield SyntacticProcessing.FAILED
                     
                 else:
-                    #print(f"({X}, {w[i]}) -> {LL1_PARSING_TABLE[(X, w[i])][1]}")
+                    #print(f"({X}, {w}) -> {LL1_PARSING_TABLE[(X, w)][1]}")
                     pilha.pop(0)
 
-                    for caracter in reversed(list(LL1_PARSING_TABLE[(X, w[i])][1])):     # LOGICA ERRADA --> Usar split(' ')??
-                    if caracter == 'epslon' or '': 
-                        continue
-                    pilha.insert(0, caracter)
+                    for caracter in reversed((LL1_PARSING_TABLE[(X, w)][1].split(' '))):     # change name of variable caracter
+                        #print(caracter)
+                        if caracter == 'epslon': 
+                            continue
+                        pilha.insert(0, caracter)
             except KeyError:
-                return False
+                yield SyntacticProcessing.FAILED
             
             X = pilha[0]
 
-        return True
+            lexem, token = yield SyntacticProcessing.IN_PROGRESS   # Still executing
+            # If it is the last iteration
+            if (token == None):       
+                w = "$"
+
+        yield SyntacticProcessing.SUCCESS
